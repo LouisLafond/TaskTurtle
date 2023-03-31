463664e430d2371958dd1f90f3ae6c096bd71f51
SINGER Jules <jules.singer@telecomnancy.eu>
2023-03-31 11:09:31 +0200

    Creation de l'architechture des pages avec Home.js qui est utilisé dans App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
+import Home from './pages/Home.js';
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
   return (
     <div class="App">
-      <header class="App-header">
-        <img src={`/images/franklin.png`} class="App-logo" alt="Logo tortue" />
-        <p>The current time is {currentTime}.</p>
-      </header>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
+  return (
+    <div class="App">
+      <header class="App-header">
+        <img src={`/images/franklin.png`} class="App-logo" alt="Logo tortue" />
+        <p>The current time is {currentTime}.</p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
