23286107ee348e9878c11ed788f2075b4bfa0760
SINGER Jules <jules.singer@telecomnancy.eu>
2023-05-11 15:48:39 +0200

    Navigation

---
+++ src/pages/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navigation() {
+
+    return (
+        <nav id='nav'>
+            <a href='/'>Accueil</a>
+            <a href='/tasks'>Services</a>
+            <a href='/tasks/create'>Proposition</a>
+        </nav>
+    );
+}
+
+export default Navigation;
