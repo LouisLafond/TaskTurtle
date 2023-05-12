bd135679cc4e54060effb7a543ef168ff21023da
SINGER Jules <jules.singer@telecomnancy.eu>
2023-05-12 16:11:43 +0200

    taches acceptées affiché seulement lorsqu'elle ne sont pas terminées

--- src/pages/UserTasks.js
+++ src/pages/UserTasks.js
@@ -34,19 +34,22 @@ function UserTasks() {
             <Navigation />
             <h1 className='text-center'> Liste des tâches de { name }</h1>
             <div id='tasks-container'>
-                {tasks.map((task) => (
-                    <div key={task[0]} id='task'>
-                        <p>fait : {task[3]}</p>
-                        <h2 className='task-title'>{task[5]}</h2> {/* title */}
-                        <p className='task-description'>{task[6]}</p> {/*description */}
-                        <p className='task-price'> Prix : {task[7]} €</p> {/*price */}
-                        <p className='task-tel'> Contact : {task[10]}</p> {/*telephone */}
-                        <p className='task-author'> Demandé par : {task[9]}</p> {/*Demandeur */}
-                        <form onSubmit={e => { endTask(e, task[0])}} method="post">
-                            <button className='button-markup'>J'ai terminé cette tâche</button>
-                        </form>
-                    </div>
-                ))}
+                {tasks.map((task) => {
+                    if(task[3]== 0) {
+                        return (
+                            <div key={task[0]} id='task'>
+                                    <h2 className='task-title'>{task[5]}</h2> 
+                                    <p className='task-description'>{task[6]}</p> 
+                                    <p className='task-price'> Prix : {task[7]} €</p> 
+                                    <p className='task-tel'> Contact : {task[10]}</p> 
+                                    <p className='task-author'> Demandé par : {task[9]}</p> 
+                                    <form onSubmit={e => { endTask(e, task[0])}} method="post">
+                                        <button className='button-markup'>J'ai terminé cette tâche</button>
+                                    </form>
+                            </div>
+                        )
+                    }
+                })}
             </div>
         </div>    
     );
